e99ca5c85e545a3a6e53c0f62f58889ce34f43e8
hzemouri <hocem.zemouri-ext@groupe-satec.com>
2023-11-29 19:02:37 +0100

    config server + swagger done

--- src/main/java/com/dovi/addressapi/controller/AddressController.java
+++ src/main/java/com/dovi/addressapi/controller/AddressController.java
@@ -5,6 +5,8 @@ import com.dovi.addressapi.dto.AddressDTO;
 import com.dovi.addressapi.service.AddressService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,10 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/addresses")
+@RefreshScope
 public class AddressController {
+
+  @Value("${address.test}")
+  private String test;
 
   @Autowired
   AddressService addressService;
+
+  @GetMapping("/test")
+  public String test() {
+    return test;
+  }
 
   @GetMapping()
   public List<AddressDTO> getAllAddresses() {
